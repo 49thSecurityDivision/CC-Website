36fcfc222f1f6aecd00a37fe22f56112d417f32c
BuildTools <wring436@gmail.com>
2022-03-22 19:52:49 -0400

    extend CFP countdown timer

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ window.onload = function(){
     placeStars(150);
     if (window.location.pathname == "/"){
         document.getElementById("con-timer").innerText = Math.ceil((new Date("2022/04/29") - new Date()) / (1000 * 60 * 60 * 24));
-        document.getElementById("cfp-timer").innerText = Math.ceil((new Date("2022/03/20") - new Date()) / (1000 * 60 * 60 * 24));
+        document.getElementById("cfp-timer").innerText = Math.ceil((new Date("2022/03/29") - new Date()) / (1000 * 60 * 60 * 24));
     }
 };
 
